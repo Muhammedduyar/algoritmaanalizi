d4ec6f9dec52dc51b70e8aa014e639f7b44519e5
Muhammedduyar <duyarmuhammed2058@gmail.com>
2025-03-23 03:06:05 +0300

    asd

--- algoritmaanalizi/Program.cs
+++ algoritmaanalizi/Program.cs
@@ -52,17 +52,61 @@ internal class Program
     
 
      */
-    public static void bubblesort() 
+    public static void bubblesort(int[] arr) 
     {
         // O(n^2)
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Değiştirme işlemi
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
-    public static void selectionsort()
+    public static void selectionsort(int[] arr)
     {
         // O(n^2) 
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Değiştirme işlemi
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
     }
-    /*
-     * max subbaray problem
-     * 
+    public static void InsertionSort(int[] arr)
+    {
+        //O(n^2)
+        int n = arr.Length;
+        for (int i = 1; i < n; i++)
+        {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key)
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+    }
+    
     
 
 }
